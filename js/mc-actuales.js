eab4313d5c5510e81210f182f74318f9ebdfde13
Lorena  Leal <lorena_lg32@hotmail.com>
2020-02-10 12:01:10 -0500

    Se agrega la funcion para mostrar el plan del cliente

--- js/mc-actuales.js
+++ js/mc-actuales.js
@@ -1,6 +1,24 @@
 const paqBaseUrl = "https://fpaq.azurewebsites.net/api/packages/";
 
-Vue.filter('formatDate', function(value) {
+/* -------- MOSTRAR CUENTA SEGUN PLAN ----------------*/
+var cuenta = dataout.N;
+var expression = /PL|BL|TL\b/g;
+var result = cuenta.match(expression);
+console.log(result[0]);
+if(result[0] == "PL"){
+  document.getElementById("idplan").innerText = "PYMEBOX BOXLUXURY";
+}else{
+  if (result[0] == "BL") {
+    document.getElementById("idplan").innerText = "BOLUXURY STANDART";
+  } else {
+    if(result[0] == "TL"){
+      document.getElementById("idplan").innerText = "BOLUXURY CLUB";
+    }
+  }
+}
+/* -------- END MOSTRAR CUENTA SEGUN PLAN ----------------*/
+
+Vue.filter('formatDate', function (value) {
   if (value) {
     return moment(String(value)).format('DD/MM/YYYY hh:mm')
   }
@@ -10,25 +28,25 @@ const vp = new Vue({
   data: {
     results: []
   },
-  mounted () {
+  mounted() {
     this.getPaqs();
   },
   methods: {
     getPaqs() {
       var dataout = JSON.parse(sessionStorage.getItem('appData'));
       let url = paqBaseUrl + dataout.C;
-      axios.get(url,    
+      axios.get(url,
         {
-            headers: {
-                "Authorization" : "Bearer " +  dataout.T
-            }
+          headers: {
+            "Authorization": "Bearer " + dataout.T
+          }
         })
-      .then((response) => {
-        // debugger;
-        this.loading = false;
-        this.results = response.data;
-        // debugger;
-      }).catch((error) => { console.log(error); });
+        .then((response) => {
+          // debugger;
+          this.loading = false;
+          this.results = response.data;
+          // debugger;
+        }).catch((error) => { console.log(error); });
     }
   }
 });
